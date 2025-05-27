98c824d3cf40e79bb9eb55f63a76de48d084f61a
Matthew Hatton <matthew.hatton@strath.ac.uk>
2025-05-27 14:22:54 +0100

    cleanup

--- R Scripts/Transient/Barents_Sea/NM/2.2 Calculate Recovery Time of Impact Date.R
+++ R Scripts/Transient/Barents_Sea/NM/2.2 Calculate Recovery Time of Impact Date.R
@@ -1,7 +1,11 @@
-## Script to calculate Demersal stock recovery time using a rolling MSY value
+## Script to calculate Demersal stock recovery time using a rolling MSY value and different recovery conditions
+## Recovery conditions are:
+## Baseline (0 fishing) ran to a steady state each year - snapshot of ecosystem. Requires 5 consecutive years equal to or above baseline
+## Baseline (0 fishing) ran in transient mode - effects from 2020. Requires intersection
 
-rm(list = ls())
+rm(list = ls()) #reset
 
+# load packages
 library(purrr)
 library(dplyr)
 library(tibble)
@@ -11,16 +15,18 @@ library(slider)
 library(zoo)
 library(progressr)
 
-progressr::handlers("debug")
+progressr::handlers("cli") # progress bars are nice
 
-
+# where are we calculating to?
 transient_years <- seq(2020,2099)
 interval <- seq(2020,2085,5)
 
+# load
 all <- readRDS("../Objects/Experiments/Rolling Crash/Rolling_Crash_and_MSY_Demersal.RDS")
 baseline <- readRDS("../Objects/Experiments/Baseline/Baseline_0_fishing_Demersal_fish.RDS")
 baseline_non_ss <- readRDS("../Objects/Experiments/Baseline/Baseline_0_fishing_Demersal_fish_1year.RDS")
 
+# extract
 baseline_df <- data.frame(
   year = transient_years[1:length(baseline[["Biomasses"]])],
   baseline = map_dbl(baseline[["Biomasses"]], ~ .x$Model_annual_mean[27])) %>% #extract DF biomass
@@ -57,16 +63,18 @@ for (i in 1:length(all)) {
                k == 3 ~ "2x MSY"
              ))
     base <- baseline_df %>% filter(year > interval[i])
+    base_non_ss <- baseline_non_ss_df %>% filter(year > interval[i])
     df <- df %>% mutate(baseline = base$baseline,
+                        baseline_non_ss = base_non_ss$baseline,
                         MSC = base$baseline * 0.8)
     master <- rbind(df,master)
   }
 }
 
-# Define parameters
-threshold_val <- 1     # E.g., 0.8 if threshold is 80% of baseline
-tolerance <- 0.05      # ±5%
-n_consecutive <- 5     # Require at least 3 consecutive years within range
+############ RECOVER TO SS
+threshold_val <- 1     # e.g 0.8 if threshold is 80% of baseline
+tolerance <- 0.01      # ±X%
+n_consecutive <- 5     # Require at least N consecutive years within range
 
 # Add threshold bands and check if values are within range OR above baseline
 master_threshold <- master %>%
@@ -91,10 +99,98 @@ recovery_baseline <- master_threshold %>%
   mutate(Recovery_Time_Baseline = year - Crash_Year) %>%
   dplyr::select(HR, Crash_Year, Recovery_Time_Baseline)
 
+## all year biomasses
+ss_biomass <- ggplot() +
+  geom_line(data = master, aes(x = year, y = Biomass, color = as.character(HR))) +
+  geom_line(
+    data = baseline_df,
+    aes(x = year, y = baseline), linetype = "solid", inherit.aes = FALSE,alpha = 0.6
+  ) +
+  geom_ribbon(
+    data = baseline_df,
+    aes(x = year, y = baseline,ymin = baseline - (baseline * 0.05),ymax = baseline + (baseline * 0.05)),alpha = 0.1) +
+  facet_wrap(~ Crash_Year, ncol = 3, scales = "free_x",strip.position = "top") +
+  labs(x = "Year", y = "Demersal Fish Biomass (mmN/m2)", color = "Harvest Rate",
+       caption = "Recovery Time decreases here due to way in which recovery time is calculated.\n Large variation in baseline greatly affects recovery time."
+  ) +
+  scale_x_continuous(limits = c(2020,2099)) +
+  theme_minimal() +
+  theme(strip.text = element_text(face = "bold"),
+        legend.position = "top",
+        legend.text = element_text(size = 12)) +
+  NULL
+ss_biomass
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 2a Recovery Time Steady State Baseline.png",
+       dpi = 1200,width = 25,unit = "cm",bg = "white",plot = ss_biomass)
 
-ggplot(recovery_baseline, aes(x = Crash_Year, y = Recovery_Time_Baseline, color = as.character(HR))) +
-  geom_line(linewidth = 1,alpha = 0.4) +
-  geom_point(size = 2,alpha = 0.4) +
+
+
+ss_recovery <- ggplot(recovery_baseline, aes(x = Crash_Year, y = Recovery_Time_Baseline, color = as.character(HR))) +
+  geom_line(linewidth = 1,alpha = 1) +
+  geom_point(size = 2,alpha = 1) +
+  # geom_smooth(se = FALSE, linewidth = 0.75) +
+  geom_hline(yintercept = 20, linetype = "dashed") +
+  labs(x = "Release Year", y = "Recovery Time (Years)", color = "Harvest Rate",
+       caption = "Recovery Time decreases here due to way in which recovery time is calculated.\n Large variation in baseline greatly affects recovery time."
+  ) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +  # y-axis starts at 0
+  theme_minimal(base_size = 14) +
+  theme(legend.position = "top") +
+  NULL
+ss_recovery
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 2b Recovery Time Steady State Baseline.png",
+       dpi = 1200,width = 25,unit = "cm",bg = "white",plot = ss_recovery)
+
+ss_biomass + ss_recovery
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 2 Steady State Baseline.png",
+       dpi = 1200,width = 35,unit = "cm",bg = "white") # will need cleaning up for publication
+
+
+
+
+############ RECOVER TO NON-SS
+tolerance <- 0.05  # ±5%
+
+# Simplified recovery calculation
+recovery_baseline <- master %>%
+  group_by(HR, Crash_Year) %>%
+  mutate(
+    lower_bound = baseline_non_ss * (1 - tolerance),
+    upper_bound = baseline_non_ss * (1 + tolerance)
+  ) %>%
+  filter(year > Crash_Year) %>%
+  filter(Biomass >= lower_bound & Biomass <= upper_bound) %>%
+  slice_min(year, with_ties = FALSE) %>%
+  mutate(Recovery_Time = year - Crash_Year) %>%
+  ungroup() %>%
+  dplyr::select(HR, Crash_Year, Recovery_Time)
+
+non_ss_biomass <- ggplot() +
+  geom_line(data = master, aes(x = year, y = Biomass, color = as.character(HR))) +
+  geom_line(
+    data = baseline_non_ss_df,
+    aes(x = year, y = baseline),inherit.aes = FALSE,alpha = 0.6,color = "black"
+  ) +
+  geom_ribbon(
+    data = baseline_non_ss_df,
+    aes(x = year, y = baseline,ymin = baseline - (baseline * 0.05),ymax = baseline + (baseline * 0.05)),alpha = 0.1) +
+  facet_wrap(~ Crash_Year, ncol = 3, scales = "free_x",strip.position = "top") +
+  labs(x = "Year", y = "Demersal Fish Biomass (mmN/m2)", color = "Harvest Rate"
+  ) +
+  scale_x_continuous(limits = c(2020,2099)) +
+  theme_minimal() +
+  theme(strip.text = element_text(face = "bold"),
+        legend.position = "top",
+        legend.text = element_text(size = 12)) +
+  NULL
+non_ss_biomass
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 3a Biomass Transient Baseline.png",
+       dpi = 1200,width = 25,unit = "cm",bg = "white",plot = non_ss_biomass)
+
+
+non_ss_recovery <- ggplot(recovery_baseline, aes(x = Crash_Year, y = Recovery_Time, color = as.character(HR))) +
+  geom_line(linewidth = 1,alpha = 1) +
+  geom_point(size = 2,alpha = 1) +
   # geom_smooth(se = FALSE, linewidth = 0.75) +
   geom_hline(yintercept = 20, linetype = "dashed") +
   labs(x = "Release Year", y = "Recovery Time (Years)", color = "Harvest Rate") +
@@ -103,78 +199,12 @@ ggplot(recovery_baseline, aes(x = Crash_Year, y = Recovery_Time_Baseline, color 
   theme(legend.position = "top") +
   NULL
 
-# ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 3 rolling MSY.png",
-#        dpi = 1200,width = 25,unit = "cm",bg = "white")
+non_ss_recovery
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 3b Recovery Time Transient Baseline.png",
+       dpi = 1200,width = 25,unit = "cm",bg = "white",plot = non_ss_recovery)
 
-ggplot() +
-  geom_line(data = filter(master,Crash_Year == 2020), aes(x = year, y = Biomass, color = as.character(HR))) +
-  geom_line(
-    data = baseline_df,
-    aes(x = year, y = baseline), linetype = "solid", inherit.aes = FALSE,alpha = 1
-  ) +
-  geom_line(
-    data = baseline_non_ss_df,
-    aes(x = year, y = baseline), linetype = "dashed", inherit.aes = FALSE,alpha = 1,color = "black"
-  ) +
-  # # geom_smooth(se = FALSE) +
-  # geom_ribbon(
-  #   data = baseline_df,
-  #   aes(x = year, y = baseline,ymin = baseline - (baseline * 0.05),ymax = baseline + (baseline * 0.05)),alpha = 0.1) +
-  facet_wrap(~ Crash_Year, ncol = 3, scales = "free_x",strip.position = "top") +
-  labs(
-    # title = "Demersal Fish Biomass Post-Crash by Crash Year and Harvest Rate",
-    x = "Year", y = "Demersal Fish Biomass (mmN/m2)", color = "Harvest Rate"
-  ) +
-  scale_x_continuous(limits = c(2020,2099)) +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold"),
-        legend.position = "top",
-        legend.text = element_text(size = 12)) +
-  NULL
-ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 2 rolling MSY BOTH 2020.png",
-       dpi = 1200,width = 25,unit = "cm",bg = "white")
-# 
-# 
-# ## EXPLORATION
-# explore <- biomass_df %>% 
-#   filter(crash == 2075)
-# ggplot(explore, aes(x = year, y = biomass, color = as.character(HR))) +
-#   geom_line(
-#     data = baseline_df,
-#     aes(x = year, y = baseline), linetype = "dashed", inherit.aes = FALSE,alpha = 0.6
-#   ) +
-#   geom_line(linewidth = 1) +
-#   labs(x = "Release Year", y = "Recovery Time (Years)", color = "Harvest Rate") +
-#   scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +  # y-axis starts at 0
-#   theme_minimal(base_size = 14) +
-#   theme(legend.position = "top") +
-#   NULL
+non_ss_biomass + non_ss_recovery
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 3 Transient Baseline.png",
+       dpi = 1200,width = 35,unit = "cm",bg = "white") # will need cleaning up for publication
 
 
-ggplot() +
-  geom_line(data = filter(master,Crash_Year == 2020 & HR == "2x MSY"), aes(x = year, y = Biomass, color = as.character(HR))) +
-  geom_line(
-    data = baseline_df,
-    aes(x = year, y = baseline), linetype = "solid", inherit.aes = FALSE,alpha = 1
-  ) +
-  geom_line(
-    data = baseline_non_ss_df,
-    aes(x = year, y = baseline), linetype = "dashed", inherit.aes = FALSE,alpha = 1,color = "black"
-  ) +
-  # # geom_smooth(se = FALSE) +
-  # geom_ribbon(
-  #   data = baseline_df,
-  #   aes(x = year, y = baseline,ymin = baseline - (baseline * 0.05),ymax = baseline + (baseline * 0.05)),alpha = 0.1) +
-  facet_wrap(~ Crash_Year, ncol = 3, scales = "free_x",strip.position = "top") +
-  labs(
-    # title = "Demersal Fish Biomass Post-Crash by Crash Year and Harvest Rate",
-    x = "Year", y = "Demersal Fish Biomass (mmN/m2)", color = "Harvest Rate"
-  ) +
-  scale_x_continuous(limits = c(2020,2099)) +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold"),
-        legend.position = "top",
-        legend.text = element_text(size = 12)) +
-  NULL
-ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Experiment/Meeting/Attractors SS and transient.png",
-       dpi = 1200,width = 25,unit = "cm",bg = "white")
