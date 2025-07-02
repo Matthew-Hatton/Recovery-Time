93e19635aa719158a4057daf1c9c63d43b3e5ccc
Matthew Hatton <matthew.hatton@strath.ac.uk>
2025-07-02 10:21:14 +0100

    spelling

--- R Scripts/Transient/Barents_Sea/NM/2.3 Max Recovery Rate.R
+++ R Scripts/Transient/Barents_Sea/NM/2.3 Max Recovery Rate.R
@@ -28,7 +28,7 @@ pfish_summary <- pfish_all_upper %>%
 ## FOCAL - PFISH
 pfish_focal <- readRDS("../Objects/Experiments/Maximum recovery time/PF_Recovery_focal.rds") %>% 
   mutate(Guild_Crash = "Planktivorous Fish",
-         Level = "Targetted Guild",
+         Level = "Targeted Guild",
          Guild = "Planktivorous Fish")
 
 ## WHOLE ECOSYSTEM - DFISH
@@ -45,7 +45,7 @@ dfish_summary <- dfish_all_upper %>%
 ## FOCAL - FISH
 dfish_focal <- readRDS("../Objects/Experiments/Maximum recovery time/DF_Recovery_focal.rds") %>% 
   mutate(Guild_Crash = "Demersal Fish",
-         Level = "Targetted Guild",
+         Level = "Targeted Guild",
          Guild = "Demersal Fish")
 
 
@@ -136,7 +136,7 @@ ggplot(facet, aes(x = Crash_Year, y = Recovery_Time)) +
   
   # Grey filled circles for Focal in NR pairs (and not 80)
   geom_point(
-    data = facet %>% filter(pair_NR, Level == "Targetted Guild", !NR_flag),
+    data = facet %>% filter(pair_NR, Level == "Targeted Guild", !NR_flag),
     shape = 16, color = "grey30", size = 4,alpha = 0.5,
     show.legend = FALSE
   ) +
@@ -147,21 +147,20 @@ ggplot(facet, aes(x = Crash_Year, y = Recovery_Time)) +
     shape = 4, color = "grey30", size = 4, stroke = 1,alpha = 0.8,
     show.legend = FALSE
   ) +
-  
-  # geom_text_repel(
-  #   data = facet %>%
-  #     filter(Level == "Whole Ecosystem", Recovery_Time != 0),
-  #   aes(label = Guild),
-  #   size = 2,
-  #   max.overlaps = Inf,
-  #   show.legend = FALSE,
-  #   box.padding = 0.3,
-  #   point.padding = 0.2,
-  #   force = 1,
-  #   force_pull = 1,
-  #   segment.alpha = 1,
-  #   segment.color = "black"
-  # ) +
+  geom_text_repel(
+    data = facet %>%
+      filter(Level == "Whole Ecosystem", Recovery_Time != 0),
+    aes(label = Guild),
+    size = 2,
+    max.overlaps = Inf,
+    show.legend = FALSE,
+    box.padding = 0.3,
+    point.padding = 0.2,
+    force = 1,
+    force_pull = 1,
+    segment.alpha = 1,
+    segment.color = "black"
+  ) +
 #median notch
 # geom_segment(
 #   data = median_points,
@@ -176,18 +175,18 @@ ggplot(facet, aes(x = Crash_Year, y = Recovery_Time)) +
 #   inherit.aes = FALSE
 # ) +
 # Add labels
-  # geom_text(
-  #   data = guild_labels,
-  #   aes(x = Crash_Year, y = y, label = Guild),
-  #   angle = 90,
-  #   size = 2,
-  #   vjust = 0.5,
-  #   color = "black",
-  #   inherit.aes = FALSE
-  # ) +
+# geom_text(
+#   data = guild_labels,
+#   aes(x = Crash_Year, y = y, label = Guild),
+#   angle = 90,
+#   size = 2,
+#   vjust = 0.5,
+#   color = "black",
+#   inherit.aes = FALSE
+# ) +
   # Add label centered below the bracket
   geom_hline(yintercept = 20, linetype = "dashed") +
-  scale_shape_manual(values = c("Targetted Guild" = 16, "Ecosystem Maximum" = 1)) +
+  scale_shape_manual(values = c("Targeted Guild" = 16, "Ecosystem Maximum" = 1)) +
   scale_x_continuous(limits = c(2020,2085),
                      breaks = c(2020,2040,2060,2080)) +
   scale_y_continuous(
@@ -211,11 +210,11 @@ ggplot(facet, aes(x = Crash_Year, y = Recovery_Time)) +
 
 
 
-ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 6.png",
+ggsave("../Figures/Transient/Barents_Sea/NM/Draft 1/Figure 5.png",
        dpi = 1200,width = 35,height = 20,unit = "cm",bg = "white") # will need cleaning up for publication
- 
+
 ## and if you're happy
-ggsave("./Figures/Figure 6.png",
+ggsave("./Figures/Figure 5.png",
        dpi = 1200,width = 35,height = 20,unit = "cm",bg = "white")
 
 
